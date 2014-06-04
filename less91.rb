d645149ce8e75901dec12e6418f7d879af79c84b
mariuszantal <mariusz.antal@gmail.com>
2014-06-04 09:05:02 +0300

    Added another simple example

---
+++ less91.rb
@@ -0,0 +1,33 @@
+p STDOUT.class
+p STDOUT.fileno
+
+p STDIN.class
+p STDIN.fileno
+
+p STDERR.class
+p STDERR.fileno
+
+# open the file "new-fd" and create a file descriptor:
+fd = IO.sysopen("new-fd", "w")
+
+# create a new I/O stream using the file descriptor for "new-fd":
+p IO.new(fd)
+
+io_streams = Array.new
+
+ObjectSpace.each_object(IO) { |x| io_streams << x }
+
+p io_streams.length
+
+p $stdin.object_id
+p STDIN.object_id
+
+puts
+
+p $stdout.object_id
+p STDOUT.object_id
+
+puts
+
+p $stderr.object_id
+p STDERR.object_id
