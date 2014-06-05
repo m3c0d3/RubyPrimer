104672fa5ef976ae6d522f39dab4b10e8d6fc6c8
m3c0d3 <m3c0d3@users.noreply.github.com>
2014-06-06 01:55:07 +0300

    Problem09

---
+++ bin/problem09.rb
@@ -0,0 +1,13 @@
+=begin
+Problem Statement
+Given an Array, return the elements that are present exactly once in the array.
+You need to write a method called non_duplicated_values to accomplish this task.
+Example: Given [1,2,2,3,3,4,5], the method should return [1,4,5]
+=end
+class Problem09
+  def non_duplicated_values(values)
+    values.find_all do |x|
+      values.count(x) == 1
+    end
+  end
+end
